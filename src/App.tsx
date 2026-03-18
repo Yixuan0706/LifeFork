73c722d8ea644d92509195b1919757f2bd50b4ff
Yixuan0706 <165487700+Yixuan0706@users.noreply.github.com>
2026-03-18 22:05:58 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
+import React from 'react';
+import { DecisionProvider, useDecision } from './contexts/DecisionContext';
 import LandingPage from './pages/LandingPage';
-import { DecisionProvider } from './contexts/DecisionContext';
+import LetterPage from './pages/LetterPage';
+import QuestionsPage from './pages/QuestionsPage';
+import ResultsPage from './pages/ResultsPage';
+
+function AppContent() {
+  const { step } = useDecision();
+
+  console.log('[AppContent] current step:', step);
+
+  switch (step) {
+    case 'landing':
+      return <LandingPage />;
+    case 'letter':
+      return <LetterPage />;
+    case 'questions':
+      return <QuestionsPage />;
+    case 'results':
+      return <ResultsPage />;
+    default:
+      return <LandingPage />;
+  }
+}
 
 export default function App() {
   return (
     <DecisionProvider>
-      <LandingPage />
+      <AppContent />
     </DecisionProvider>
   );
 }
